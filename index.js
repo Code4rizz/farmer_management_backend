833d4ada59e88527e7d9a176227a10d502cc5fa9
Code4rizz <137693232+Code4rizz@users.noreply.github.com>
2025-09-04 02:53:29 +0530

    updated

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.get('/', (req, res) => {
     res.send('API is running...');});
 
     
-mongoose.connect('mongodb+srv://ADITHYAN:koovappady@farmer.xo1uvwb.mongodb.net/?retryWrites=true&w=majority&appName=Farmer')
+mongoose.connect(process.env.MONGO_URI,{ useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('MongoDB connected'))
     .catch(err => console.log(err));
 
