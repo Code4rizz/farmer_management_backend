69684d7807e0d428173fb6da45377bfd800e459f
Code4rizz <137693232+Code4rizz@users.noreply.github.com>
2025-09-04 03:05:54 +0530

    updated

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors({
-  origin: "https://farmer-management-frontend-fawn.vercel.app/", // your frontend URL
+  origin: "https://farmer-management-frontend-fawn.vercel.app", // your frontend URL
   credentials: true,
 }));
 
@@ -26,12 +26,12 @@ app.get('/', (req, res) => {
     res.send('API is running...');});
 
     
-mongoose.connect(process.env.MONGO_URI,{ useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGO_URI)
     .then(() => console.log('MongoDB connected'))
     .catch(err => console.log(err));
 
 
-const PORT= 5000;
+const PORT= process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
